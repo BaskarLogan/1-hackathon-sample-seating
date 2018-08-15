33e755dc49a53f73b118e839329b8d6c5a093788
ih864c <ih864c@A6104629.nw.nos.boeing.com>
2018-08-14 20:57:21 -0700

    updated

--- src/main/resources/db/migration/V2__seeddata.sql
+++ src/main/resources/db/migration/V2__seeddata.sql
@@ -1,5 +1,5 @@
-insert into section (id, name, layout_info) values(1, '101', '___fffffff___,__fffffffff__,__fffffffff__,__eeeeeeeee__,__eeeeeeeee__,_eeeeeeeeeee_,_eeeeeeeeeee_,eeeeeeeeeeeee,eeeeeeeeeeeee');
-insert into section (id, name, layout_info) values(2, '102', '____fffff____,___fffffff___,__fffffffff__,__eeeeeeeee__,__eeeeeeeee__,_eeeeeeeeeee_,_eeeeeeeeeee_,eeeeeeeeeeeee,eeeeeeeeeeeee');
+insert into section (id, name, layout_info) values(1, '103', '___fffffff___,__fffffffff__,__fffffffff__,__eeeeeeeee__,__eeeeeeeee__,_eeeeeeeeeee_,_eeeeeeeeeee_,eeeeeeeeeeeee,eeeeeeeeeeeee');
+insert into section (id, name, layout_info) values(2, '104', '____fffff____,___fffffff___,__fffffffff__,__eeeeeeeee__,__eeeeeeeee__,_eeeeeeeeeee_,_eeeeeeeeeee_,eeeeeeeeeeeee,eeeeeeeeeeeee');
 insert into section (id, name, layout_info) values(2, '105', '____fffff____,___fffffff___,__fffffffff__,__eeeeeeeee__,__eeeeeeeee__,_eeeeeeeeeee_,_eeeeeeeeeee_,eeeeeeeeeeeee,eeeeeeeeeeeee');
 
 insert into seat (id, name, section_id, available) values (1, '1_6', 1, TRUE);
